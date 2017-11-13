881ea2a7ded631dc203dfb3f31de0eee3c081211
BiHuaI <1398125482@qq.com>
2017-11-13 21:32:30 +0800

    张三

--- src/main/java/com/zking/test/test.java
+++ src/main/java/com/zking/test/test.java
@@ -3,9 +3,7 @@ package com.zking.test;
 public class test {
     public void test(){
         System.out.println("第一次git");
-        System.out.println("怎么回事");
         System.out.println("第二行代码");
-        System.out.println("张三：这是第三行代码");
         System.out.println("李四：这是第三行代码");
     }
 }
